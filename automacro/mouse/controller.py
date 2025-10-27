61d13d9d048533b1cd7fba956161d4644d92eea6
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-10-26 23:01:02 -0400

    feat(mouse): implement controller

---
+++ automacro/mouse/controller.py
@@ -0,0 +1,62 @@
+from enum import Enum
+
+from pynput.mouse import Button, Controller
+
+
+class MouseButton(Enum):
+    LEFT = Button.left
+    RIGHT = Button.right
+    MIDDLE = Button.middle
+
+    def __eq__(self, other):
+        if isinstance(other, Button):
+            return self.value == other
+        return super().__eq__(other)
+
+
+class MouseController:
+    """
+    A controller for mouse actions.
+    """
+
+    def __init__(self):
+        """
+        Initialize the mouse controller.
+        """
+
+        self._controller = None
+
+    def move(self, x: int, y: int) -> None:
+        """
+        Move the mouse to the specified (x, y) coordinates.
+
+        Args:
+            x (int): x-coordinate to move to.
+            y (int): y-coordinate to move to.
+        """
+
+        if not self._controller:
+            self._controller = Controller()
+        self._controller.position = (x, y)
+
+    def click(self, button: MouseButton, count: int = 1) -> None:
+        """
+        Click the specified mouse button a given number of times.
+
+        Args:
+            button (MouseButton): Mouse button to click.
+            count (int): Number of times to click the button. Default is 1.
+        """
+
+        if not self._controller:
+            self._controller = Controller()
+        self._controller.click(button.value, count)
+
+    def scroll(self, dx: int, dy: int) -> None:
+        """
+        Scroll the mouse by the specified amounts in the x and y directions.
+        """
+
+        if not self._controller:
+            self._controller = Controller()
+        self._controller.scroll(dx, dy)
