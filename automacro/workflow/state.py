79672fc9f0471d16e17bd62c1dc13cc7abd9a4d7
Jun Kai Zhang <junkaizhang8@gmail.com>
2025-12-27 23:48:07 -0500

    doc(workflow): add docstring to `WorkflowState`

--- automacro/workflow/state.py
+++ automacro/workflow/state.py
@@ -2,6 +2,10 @@ from enum import Enum, auto
 
 
 class WorkflowState(Enum):
+    """
+    An enumeration representing the state of a workflow.
+    """
+
     IDLE = auto()
     RUNNING = auto()
     PAUSED = auto()
